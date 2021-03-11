06f474e8044d07e4dda96ba2b7882d980de3ff54
daxus4 <davide.raffaellii@gmail.com>
2021-03-11 17:57:15 +0100

    Score

--- src/Score.java
+++ src/Score.java
@@ -1,10 +1,10 @@
 public class Score {
 
-    public static int score(Antenna a, Building b) {
-        return b.getConnection_Speed_Weight()*a.getConnSpeed() - b.getLatency_Weight()*manDistance(a,b);
+    public static int score(Antenna a, Building b, Coordinate antCoord, Coordinate buildCoord) {
+        return b.getConnection_Speed_Weight()*a.getConnSpeed() - b.getLatency_Weight()*manDistance(antCoord,buildCoord);
     }
 
-    public static int manDistance(Antenna a, Building b){
-        return 0;
+    public static int manDistance(Coordinate a, Coordinate b){
+        return a.getX()-b.getX() + a.getY()+b.getY();
     }
 }
